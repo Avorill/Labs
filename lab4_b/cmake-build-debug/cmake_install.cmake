137252f70672ef4786dc6aae4f53dc33a9dbbf71
POZIT1V <brotgars@mail.ru>
2020-10-09 23:34:55 +0300

    project b last version

--- lab4_b/cmake-build-debug/cmake_install.cmake
+++ lab4_b/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/c5066469/Desktop/reposit/Labs/lab4_b
+# Install script for directory: C:/Users/c5066469/Desktop/lab4_b
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/c5066469/Desktop/reposit/Labs/lab4_b/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/c5066469/Desktop/lab4_b/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
