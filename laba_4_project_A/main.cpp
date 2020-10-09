009c6729c9d45542908ef9eddc97fea66de76a50
POZIT1V <brotgars@mail.ru>
2020-10-09 10:40:42 +0300

    project_A

---
+++ laba_4_project_A/main.cpp
@@ -0,0 +1,51 @@
+///программа должна демонстрировать возможности работы
+/// с разработанной Вами функцией для работы со строками
+///написать собственную реализацию strcspn( возвращает длину наяальной подстроки из string,
+/// не содержащей символов из множества strCharSet
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int _strcspn(const char *string, const char *strCharSet) {
+  int n=0;
+  const char *copy;
+  while(*string!='\0') {
+	for (copy = strCharSet; *copy!='\0'; copy++) {
+	  if (*string==*copy) {
+		return n;
+	  }
+	}
+	n++;
+	string++;
+  }
+  return n;
+}
+
+//void slovo(){
+//  char Letters
+//}
+
+
+
+int main() {
+
+   char Text[300];
+   char TextCheck[300];
+   ifstream  fin("Poroject_A.txt");
+   while(fin>>Text) {
+   if(!fin.is_open())
+     cout<<"can't read file, sorry\n";
+   else
+   {
+
+	 fin >> TextCheck;
+	 cout << Text << endl;
+	 cout << TextCheck << endl;
+	 cout<<_strcspn(Text,TextCheck)<<endl;
+   }
+   }
+   fin.close();
+
+  return 0;
+}
