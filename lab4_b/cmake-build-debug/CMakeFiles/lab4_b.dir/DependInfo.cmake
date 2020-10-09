36b4ee8c35287f43e22de7baad083121fe2d4b27
POZIT1V <brotgars@mail.ru>
2020-10-09 23:40:43 +0300

    delete trash

--- lab4_b/cmake-build-debug/CMakeFiles/lab4_b.dir/DependInfo.cmake
+++ lab4_b/cmake-build-debug/CMakeFiles/lab4_b.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/c5066469/Desktop/reposit/Labs/lab4_b/main.cpp" "C:/Users/c5066469/Desktop/reposit/Labs/lab4_b/cmake-build-debug/CMakeFiles/lab4_b.dir/main.cpp.obj"
+  "C:/Users/c5066469/Desktop/lab4_b/main.cpp" "C:/Users/c5066469/Desktop/lab4_b/cmake-build-debug/CMakeFiles/lab4_b.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "MSVC")
 
